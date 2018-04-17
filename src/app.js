b3a814f168abeabc7e12686aa70d21bbdbf04d5d
ltnsee <ltnsee8@163.com>
2018-04-17 23:37:05 +0800

    'webpack_config'

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
-console.log('hello webpack!')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+import './index.scss';
+
+class HelloMessage extends React.Component {
+    render() {
+      return (
+        <div>
+          Hello {this.props.name}
+        </div>
+      );
+    }
+  }
+  
+  ReactDOM.render(
+    <HelloMessage name="Taylor" />,
+    document.getElementById('app')
+  );
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,12 @@
  */
 const path =require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractSass = new ExtractTextPlugin({
+    filename: "index.css",
+    disable: process.env.NODE_ENV === "development"
+});
 
 module.exports = {
     entry: './src/app.js',
@@ -10,9 +16,39 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
+    module: {
+        rules: [
+            {
+                test: /\.js$|\.jsx$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env','react']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                  fallback: "style-loader",
+                  use: "css-loader"
+                })
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: ["css-loader","sass-loader"]
+                })
+            }
+        ]
+    },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './src/index.html'
-        })
+            hash:true,
+            template:'./src/index.html'
+        }),
+        new ExtractTextPlugin({filename: "index.css",})
     ]
 }
