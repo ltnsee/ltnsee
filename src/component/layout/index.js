d5efc3258309a593f4ac9af2e01f3e2fb8dad41e
ltnsee <ltnsee8@163.com>
2018-04-26 23:53:26 +0800

    导航

---
+++ src/component/layout/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import NavTop from 'component/nav-top/index.js';
+import NavSide from 'component/nav-side/index.js';
+import './theme.css'
+
+class Layout extends React.Component{
+    constructor(...args){
+        super(...args);
+    }
+    render(){
+
+        return (
+            <div id="wrapper">
+                <NavTop />
+                <NavSide />
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+export default Layout;
